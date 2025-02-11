fe2ac3b86b51423a32e1739f2d98855fc662d11a
DimensionSTP <ddang8jh@gmail.com>
2025-02-11 18:33:50 +0900

    chore: default model types

--- src/models/llm.py
+++ src/models/llm.py
@@ -8,13 +8,13 @@ from langchain_openai import ChatOpenAI, OpenAI
 
 
 CHATLLM = ChatOpenAI(
-    model="gpt-4",
+    model="gpt-4o",
     temperature=0,
     openai_api_key=os.getenv("OPENAI_API_KEY"),
 )
 
 BASELLM = OpenAI(
-    model="gpt-4",
+    model="gpt-4o",
     temperature=0,
     openai_api_key=os.getenv("OPENAI_API_KEY"),
 )
