01f97869fc1d71f04505a42daf1a8e0dd1edf3f0
DimensionSTP <ddang8jh@gmail.com>
2025-02-18 14:38:50 +0900

    delete: unused func

--- src/models/llm.py
+++ src/models/llm.py
@@ -12,9 +12,3 @@ CHATLLM = ChatOpenAI(
     temperature=0,
     openai_api_key=os.getenv("OPENAI_API_KEY"),
 )
-
-BASELLM = OpenAI(
-    model="gpt-4o",
-    temperature=0,
-    openai_api_key=os.getenv("OPENAI_API_KEY"),
-)
